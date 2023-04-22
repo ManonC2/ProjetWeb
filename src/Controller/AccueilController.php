55759578261a21b1eabc7f930253b2198efecd77
ManonC2 <manon.cvre@gmail.com>
2023-04-22 10:13:51 +0200

    Proprification du code 2

--- src/Controller/AccueilController.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class AccueilController {
-    public function index() {
-        
-    }
-}
-?>
---
+++ src/Controller/BaseController.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace Application\Controller\BaseController;
+
+class BaseController {
+    public function index() {
+        
+    }
+}
