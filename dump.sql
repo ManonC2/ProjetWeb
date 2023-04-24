7aee5306de7e015bf117bde16e69cf06724f3601
LelazoLeLezard <103433333+LelazoLeLezard@users.noreply.github.com>
2023-04-24 11:44:56 +0200

    Correction dump.sql

--- dump.sql
+++ dump.sql
@@ -1,132 +1,107 @@
+DROP TABLE IF EXISTS `ContratLabo`;
+DROP TABLE IF EXISTS `ContratVacataire`;
+DROP TABLE IF EXISTS `SiteEntreprise`;
+DROP TABLE IF EXISTS `ContratSA`;
+DROP TABLE IF EXISTS `Employe`;
+DROP TABLE IF EXISTS `Donation`;
+DROP TABLE IF EXISTS `Site`;
+DROP TABLE IF EXISTS `Entreprise`;
+DROP TABLE IF EXISTS `Etudiant`;
+
 CREATE TABLE
     `Etudiant` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `numeroEtudiant` varchar(255),
-        `nationalite` varchar(255)
+        `nationalite` varchar(255),
+        CONSTRAINT `etudiant_pk` PRIMARY KEY (`id`)
     );
 
 CREATE TABLE
     `Entreprise` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `nom` varchar(255),
         `bloque` boolean,
         `nbEmployes` integer,
         `ouvert` boolean,
-        `siege` varchar(255)
+        `siege` varchar(255),
+        CONSTRAINT `entreprise_pk` PRIMARY KEY (`id`)
     );
 
 CREATE TABLE
     `Site` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `adresse` varchar(255),
         `ville` varchar(255),
-        `pays` varchar(255)
+        `pays` varchar(255),
+        CONSTRAINT `site_pk` PRIMARY KEY (`id`)
     );
 
 CREATE TABLE
     `Donation` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `valeur` integer,
         `annee` date,
-        `entreprise_id` int
-    );
-
-CREATE TABLE
-    `ContratSA` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
-        `dateDebut` date,
-        `dateFinPrevue` date,
-        `dateFinAnticipee` date,
-        `etudiant_id` varchar(255),
-        `employe_id` varchar(255),
-        `site_id` int,
-        `type` boolean,
-        `noteEntr` int,
-        `noteMA` int
+        `entreprise_id` int,
+        CONSTRAINT `donation_pk` PRIMARY KEY (`id`),
+        CONSTRAINT `donation_fk_entreprise` FOREIGN KEY (`entreprise_id`) REFERENCES `Entreprise` (`id`)
     );
 
 CREATE TABLE
     `Employe` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `nom` varchar(255),
-        `entreprise_id` int
+        `entreprise_id` int,
+        CONSTRAINT `employe_pk` PRIMARY KEY (`id`),
+        CONSTRAINT `employe_fk_entrprise` FOREIGN KEY (`entreprise_id`) REFERENCES `Entreprise` (`id`)
+    );
+
+CREATE TABLE
+    `ContratSA` (
+        `id` int AUTO_INCREMENT,
+        `dateDebut` date,
+        `dateFinPrevue` date,
+        `dateFinAnticipee` date,
+        `etudiant_id` int,
+        `employe_id` int,
+        `site_id` int,
+        `type` boolean,
+        `noteEntr` int,
+        `noteMA` int,
+        CONSTRAINT `contratsa_pk` PRIMARY KEY (`id`),
+        CONSTRAINT `contratsa_fk_etudiant` FOREIGN KEY (`etudiant_id`) REFERENCES `Etudiant` (`id`),
+        CONSTRAINT `contratsa_fk_employe` FOREIGN KEY (`employe_id`) REFERENCES `Employe` (`id`),
+        CONSTRAINT `contratsa_fk_site` FOREIGN KEY (`site_id`) REFERENCES `Site` (`id`)
     );
 
 CREATE TABLE
     `SiteEntreprise` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
         `site_id` int,
-        `entreprise_id` int
+        `entreprise_id` int,
+        CONSTRAINT `siteentreprise_pk` PRIMARY KEY (`site_id`,`entreprise_id`),
+        CONSTRAINT `siteentreprise_fk_entreprise` FOREIGN KEY (`entreprise_id`) REFERENCES `Entreprise` (`id`),
+        CONSTRAINT `siteentreprise_fk_site` FOREIGN KEY (`site_id`) REFERENCES `Site` (`id`)
     );
 
 CREATE TABLE
     `ContratVacataire` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `dateDebut` date,
         `dateFin` date,
         `employe_id` int,
-        `note` int
+        `note` int,
+        CONSTRAINT `contratvacataire_pk` PRIMARY KEY (`id`),
+        CONSTRAINT `contratvacataire_fk_employe` FOREIGN KEY (`employe_id`) REFERENCES `Employe` (`id`)
     );
 
 CREATE TABLE
     `ContratLabo` (
-        `id` int PRIMARY KEY AUTO_INCREMENT,
+        `id` int AUTO_INCREMENT,
         `dateDebut` date,
         `dateFin` date,
-        `entreprise_id` int,
         `laboratoire_id` int,
         `employe_id` int,
-        `note` int
+        `note` int,
+        CONSTRAINT `contratlabo_pk` PRIMARY KEY (`id`),
+        CONSTRAINT `contratlabo_fk_entreprise` FOREIGN KEY (`laboratoire_id`) REFERENCES `Entreprise` (`id`),
+        CONSTRAINT `contratlabo_fk_employe` FOREIGN KEY (`employe_id`) REFERENCES `Employe` (`id`)
     );
-
-ALTER TABLE `Etudiant`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratSA` (`etudiant_id`);
-
-ALTER TABLE `Employe`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratSA` (`employe_id`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `SiteEntreprise` (`entreprise_id`);
-
-ALTER TABLE `Site`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `SiteEntreprise` (`site_id`);
-
-ALTER TABLE `Site`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratSA` (`site_id`);
-
-ALTER TABLE `Employe`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratLabo` (`employe_id`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`siege`) REFERENCES `Site` (`adresse`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `Donation` (`entreprise_id`);
-
-ALTER TABLE `Employe`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratVacataire` (`employe_id`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`nom`) REFERENCES `ContratLabo` (`entreprise_id`);
-
-ALTER TABLE `Employe`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratLabo` (`id`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `Employe` (`entreprise_id`);
-
-ALTER TABLE `Entreprise`
-ADD
-    FOREIGN KEY (`id`) REFERENCES `ContratLabo` (`laboratoire_id`);
