2167c8fee4d329556cf8d83a39247166550694f5
Manon <manon.cvre@gmail.com>
2023-04-25 09:01:04 +0200

    ContratVacataire done

--- src/Entity/ContratVacataire.php
+++ src/Entity/ContratVacataire.php
@@ -2,13 +2,107 @@
 
 namespace Application\Entity;
 
+use DateTime;
+
 class ContratVacataire {
     #region Attributs
-
+    private int $id;
+    private DateTime $dateDebut;
+    private DateTime $dateFin;
+    private int $employeId;
+    private int $entrepriseId;
+    private int $note;
     #endregion
 
     #region Getters/Setters 
 
     #endregion
 
+
+	/**
+	 * @return int
+	 */
+	public function getId(): int {
+		return $this->id;
+	}
+
+	/**
+	 * @return DateTime
+	 */
+	public function getDateFin(): DateTime {
+		return $this->dateFin;
+	}
+	
+	/**
+	 * @param DateTime $dateFin 
+	 * @return self
+	 */
+	public function setDateFin(DateTime $dateFin): self {
+		$this->dateFin = $dateFin;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getEntrepriseId(): int {
+		return $this->entrepriseId;
+	}
+	
+	/**
+	 * @param int $entrepriseId 
+	 * @return self
+	 */
+	public function setEntrepriseId(int $entrepriseId): self {
+		$this->entrepriseId = $entrepriseId;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getNote(): int {
+		return $this->note;
+	}
+	
+	/**
+	 * @param int $note 
+	 * @return self
+	 */
+	public function setNote(int $note): self {
+		$this->note = $note;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getEmployeId(): int {
+		return $this->employeId;
+	}
+	
+	/**
+	 * @param int $employeId 
+	 * @return self
+	 */
+	public function setEmployeId(int $employeId): self {
+		$this->employeId = $employeId;
+		return $this;
+	}
+
+	/**
+	 * @return DateTime
+	 */
+	public function getDateDebut(): DateTime {
+		return $this->dateDebut;
+	}
+	
+	/**
+	 * @param DateTime $dateDebut 
+	 * @return self
+	 */
+	public function setDateDebut(DateTime $dateDebut): self {
+		$this->dateDebut = $dateDebut;
+		return $this;
+	}
 }
